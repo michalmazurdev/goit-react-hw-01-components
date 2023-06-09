654f851262f8e591d39e739417949e35290f6220
michalmazurdev <m.mazur40@gmail.com>
2023-06-10 00:47:47 +0200

    2nd task code refactored

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -10,47 +10,22 @@ export const Statistics = ({ title, stats }) => (
   <section className={css.statistics}>
     {title && <h2 className={css.title}>{title}</h2>}
     <ul className={css.statList}>
-      <li
-        className={css.item}
-        style={{
-          backgroundColor: `#${randomColor()}`,
-        }}
-      >
-        <span className={css.label}>{stats[0].label}</span>
-        <span className={css.percentage}>{stats[0].percentage}%</span>
-      </li>
-      <li
-        className={css.item}
-        style={{
-          backgroundColor: `#${randomColor()}`,
-        }}
-      >
-        <span className={css.label}>{stats[1].label}</span>
-        <span className={css.percentage}>{stats[1].percentage}%</span>
-      </li>
-      <li
-        className={css.item}
-        style={{
-          backgroundColor: `#${randomColor()}`,
-        }}
-      >
-        <span className={css.label}>{stats[2].label}</span>
-        <span className={css.percentage}>{stats[2].percentage}%</span>
-      </li>
-      <li
-        className={css.item}
-        style={{
-          backgroundColor: `#${randomColor()}`,
-        }}
-      >
-        <span className={css.label}>{stats[3].label}</span>
-        <span className={css.percentage}>{stats[3].percentage}%</span>
-      </li>
+      {stats.map(stat => (
+        <li
+          className={css.item}
+          style={{
+            backgroundColor: `#${randomColor()}`,
+          }}
+        >
+          <span className={css.label}>{stat.label}</span>
+          <span className={css.percentage}>{stat.percentage}%</span>
+        </li>
+      ))}
     </ul>
   </section>
 );
 
 Statistics.propTypes = {
   title: PropTypes.string,
-  stats: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  stats: PropTypes.array,
 };
